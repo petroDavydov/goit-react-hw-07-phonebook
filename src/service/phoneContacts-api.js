7a65b3bfa285d7553a96e51d7cbfcf1e9f8fd8db
petroDavydov <petrodavydov@gmail.com>
2021-11-20 18:09:39 +0200

    +

--- src/service/phoneContacts-api.js
+++ src/service/phoneContacts-api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // дефолтний URL
-axios.defaults.baseURL = "https://61962ffbaf46280017e7debe.mockapi.io/Phonebook-contacts/";
+axios.defaults.baseURL = "https://61962ffbaf46280017e7debe.mockapi.io/phonebook-contacts";
 // Запрос
 export async function fetchContacts() {
   const { data } = await axios.get("/contacts");
