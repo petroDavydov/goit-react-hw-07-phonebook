ac69adfced3ed7b5ca57fc9adbc6311dde36c7b9
petroDavydov <petrodavydov@gmail.com>
2021-11-20 20:24:23 +0200

    change axios

--- src/service/phoneContacts-api.js
+++ src/service/phoneContacts-api.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 // дефолтний URL
-axios.defaults.baseURL = "https://61962ffbaf46280017e7debe.mockapi.io/phonebook-contacts";
+axios.defaults.baseURL = "https://61962ffbaf46280017e7debe.mockapi.io";
 // Запрос
 export async function fetchContacts() {
-  const { data } = await axios.get("/contacts");
+  const { data } = await axios.get("/phonebook-contacts");
   return data;
 }
 // Добавление
 export async function addContact(contact) {
-  const { data } = await axios.post("/contacts", contact);
+  const { data } = await axios.post("/phonebook-contacts", contact);
   return data;
 }
 // Удаление по Id
 export async function deleteContact(id) {
-  await axios.delete(`/contacts/${id}`);
+  await axios.delete(`/phonebook-contacts/${id}`);
   return id;
 }
